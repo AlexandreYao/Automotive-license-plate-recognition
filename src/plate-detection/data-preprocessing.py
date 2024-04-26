6ee5ea5257bf4422151d2fb0038a7ed67c764576
Alexandre <ykoffialexandre@gmail.com>
2024-04-26 11:07:03 +0200

    images preprocessing

---
+++ src/plate-detection/data-preprocessing.py
@@ -0,0 +1,61 @@
+import os
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class CustomDataset(Dataset):
+    def __init__(self, image_folder, annotation_folder, image_size=416):
+        self.image_folder = image_folder
+        self.annotation_folder = annotation_folder
+        self.image_size = image_size
+        self.data = self.preprocess_data()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image_path, annotations = self.data[idx]
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (self.image_size, self.image_size))
+        image = torch.tensor(image, dtype=torch.float32).permute(2, 0, 1) / 255.0
+        annotations = torch.tensor(annotations, dtype=torch.float32)
+        return image, annotations
+
+    def load_annotations(self, annotation_file):
+        with open(annotation_file, "r") as f:
+            lines = f.readlines()
+        annotations = []
+        for line in lines:
+            parts = line.strip().split()
+            annotations.append([float(parts[i]) for i in range(1, len(parts))])
+        return np.array(annotations)
+
+    def preprocess_data(self):
+        data = []
+        for image_name in os.listdir(self.image_folder):
+            if image_name.endswith(".jpg"):
+                image_path = os.path.join(self.image_folder, image_name)
+                annotation_name = os.path.splitext(image_name)[0] + ".txt"
+                annotation_path = os.path.join(self.annotation_folder, annotation_name)
+                if os.path.exists(annotation_path):
+                    annotations = self.load_annotations(annotation_path)
+                    data.append((image_path, annotations))
+        return data
+
+
+def create_dataloader(image_folder, annotation_folder, batch_size=32, image_size=416):
+    dataset = CustomDataset(image_folder, annotation_folder, image_size=image_size)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return dataloader
+
+
+# Exemple d'utilisation :
+image_folder = "data/images"
+annotation_folder = "data/annotations"
+batch_size = 32
+image_size = 416
+dataloader = create_dataloader(image_folder, annotation_folder, batch_size, image_size)
+# Maintenant vous pouvez itérer sur dataloader pour entraîner votre modèle
